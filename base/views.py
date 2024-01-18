ce32a6a29378df4fde2b0c75e52c8ad6ca41b15d
Qayyum <qayyumabdul660@gmail.com>
2024-01-18 21:57:23 +0530

    templates added

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
